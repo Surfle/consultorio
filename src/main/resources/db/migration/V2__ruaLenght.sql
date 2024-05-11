fd075d6b1b2e41edb458d61a7f1444cd438faf21
rubens.prado <109166405+Surfle@users.noreply.github.com>
2024-05-10 23:18:17 -0300

    Incluso flyway

---
+++ src/main/resources/db/migration/V2__ruaLenght.sql
@@ -0,0 +1,25 @@
+CREATE TABLE endereco (
+    id SERIAL PRIMARY KEY,
+    estado VARCHAR(50),
+    rua VARCHAR(50),
+    numero BIGINT,
+    cep VARCHAR(9)
+);
+
+CREATE TABLE paciente (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    cpf VARCHAR(14) NOT NULL,
+    email VARCHAR(100),
+    telefone VARCHAR(20),
+    endereco_id BIGINT,
+    FOREIGN KEY (endereco_id) REFERENCES endereco(id)
+);
+
+CREATE TABLE consulta (
+    id SERIAL PRIMARY KEY,
+    data DATE NOT NULL,
+    hora TIME NOT NULL,
+    paciente_id BIGINT,
+    FOREIGN KEY (paciente_id) REFERENCES paciente(id)
+);
